e3da7725be7507bb784559c6f0bd44a238c999a1
toesbieya <1647775459@qq.com>
2021-01-14 00:04:56 +0800

    修复BackToTop的图标渲染问题

--- src/component/Page/component/BackToTop.vue
+++ src/component/Page/component/BackToTop.vue
@@ -1,11 +1,13 @@
 <script type="text/jsx">
+import {Const} from "el-admin-layout"
+
 export default {
     functional: true,
 
-    render() {
+    render(h) {
         return (
             <el-backtop target=".page-main .scroll-container" visibility-height={400} bottom={66}>
-                <v-icon icon="el-icon-top"/>
+                {Const.iconRenderer(h, 'el-icon-top')}
             </el-backtop>
         )
     }
