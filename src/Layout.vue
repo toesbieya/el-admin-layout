988d89ae3afc55036b602a68aafba77b0f1f83c0
toesbieya <1647775459@qq.com>
2021-01-27 13:22:27 +0800

    Layout改为有状态，provide自身

--- src/Layout.vue
+++ src/Layout.vue
@@ -4,29 +4,14 @@ import Navbar from './component/Navbar'
 import Page from './component/Page'
 import {appGetters, pageGetters, tagsViewGetters} from "./store"
 
-function getSlotsForChild(scopedSlots, childNames) {
-    const result = childNames.reduce((obj, name) => {
-        obj[name] = Object.create(null)
-        return obj
-    }, Object.create(null))
-
-    if (!scopedSlots) return result
-
-    Object.entries(scopedSlots).forEach(([k, v]) => {
-        const childName = childNames.find(i => k.startsWith(i))
-        if (childName) {
-            const lowerCase = k.charAt(childName.length).toLowerCase() + k.slice(childName.length + 1)
-            result[childName][lowerCase] = v
-        }
-    })
-
-    return result
-}
-
 export default {
     name: 'ElAdminLayout',
 
-    functional: true,
+    provide() {
+        return {
+            elAdminLayout: this
+        }
+    },
 
     props: {
         //传递给nav-bar的props
@@ -39,32 +24,35 @@ export default {
         pageProps: Object
     },
 
-    render(h, context) {
-        const {navbarProps, asideProps, pageProps} = context.props
-        const childSlots = getSlotsForChild(context.scopedSlots, ['navbar', 'aside', 'page'])
+    computed: {
+        isLeftRight() {
+            return pageGetters.position === 'left-right'
+        },
+        renderAside() {
+            return appGetters.isMobile || ['aside', 'aside-two-part', 'mix'].includes(appGetters.navMode)
+        }
+    },
 
-        const isLeftRight = pageGetters.position === 'left-right'
-        const renderAside = appGetters.isMobile || ['aside', 'aside-two-part', 'mix'].includes(appGetters.navMode)
-
-        const aside = renderAside && <Aside {...{props: asideProps, scopedSlots: childSlots.aside}}/>
-        const navbar = <Navbar {...{props: navbarProps, scopedSlots: childSlots.navbar}}/>
+    render() {
+        const aside = this.renderAside && <Aside {...{props: this.asideProps}}/>
+        const navbar = <Navbar {...{props: this.navbarProps}}/>
 
         return (
             <section class={{
                 'app-wrapper': true,
-                'flex-column': !isLeftRight
+                'flex-column': !this.isLeftRight
             }}>
-                {isLeftRight ? aside : navbar}
+                {this.isLeftRight ? aside : navbar}
 
                 <section class={{
                     'main-container': true,
                     'has-nav': true,
                     [`nav-mode-${appGetters.navMode}`]: true,
-                    'flex-column': isLeftRight,
+                    'flex-column': this.isLeftRight,
                     'has-tags-view': tagsViewGetters.enabled
                 }}>
-                    {isLeftRight ? navbar : aside}
-                    <Page {...{props: pageProps, scopedSlots: childSlots.page}}/>
+                    {this.isLeftRight ? navbar : aside}
+                    <Page {...{props: this.pageProps}}/>
                 </section>
             </section>
         )
