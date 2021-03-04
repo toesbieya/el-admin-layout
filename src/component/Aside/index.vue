ef94e78cf93da11d58a7d5de586013e660e3fa6a
toesbieya <1647775459@qq.com>
2021-03-04 23:50:28 +0800

    侧边栏代码优化

--- src/component/Aside/index.vue
+++ src/component/Aside/index.vue
@@ -1,15 +1,22 @@
 <script>
-import {asideGetters} from "el-admin-layout"
+import {appGetters, asideGetters} from "el-admin-layout"
 import DefaultSidebar from './DefaultSidebar'
 
 export default {
     name: 'Aside',
 
+    computed: {
+        //移动端下不能设置z-index，否则会被el-drawer的遮罩遮住
+        style() {
+            return appGetters.isMobile ? 'z-index: unset !important' : undefined
+        }
+    },
+
     render() {
         const {default: slot} = this.$scopedSlots
 
         return (
-            <aside class={`aside ${asideGetters.theme}`}>
+            <aside class={`aside ${asideGetters.theme}`} style={this.style}>
                 {slot ? slot() : <DefaultSidebar ref="default-sidebar"/>}
             </aside>
         )
