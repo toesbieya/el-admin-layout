5a905ab66c725b6a66a3c88b6529f778bd249d09
toesbieya <1647775459@qq.com>
2021-03-05 12:33:58 +0800

    修复引用路径问题

--- src/component/Page/component/Iframe/index.vue
+++ src/component/Page/component/Iframe/index.vue
@@ -15,8 +15,8 @@
 
 <script>
 import {pageGetters, pageMutations, tagsViewGetters} from "el-admin-layout"
-import {getRouterKey} from "@/config/logic"
-import {isEmpty} from "@/util"
+import {getRouterKey} from "el-admin-layout/src/config/logic"
+import {isEmpty} from "el-admin-layout/src/util"
 
 export default {
     name: "PageIframe",
