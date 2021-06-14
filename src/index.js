1071fd2511916d5b6489f57c3207f642bb611ad4
toesbieya <1647775459@qq.com>
2021-06-14 20:07:57 +0800

    导出部分组件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Layout from "./component/Layout"
-import {refreshPage, closeCurrentPage} from "./helper"
+import Breadcrumb from "./component/Breadcrumb"
+import ContextMenu from "./component/ContextMenu"
+import Hamburger from "./component/Hamburger"
+import HorizontalResizableMenu from "./component/HorizontalResizableMenu"
+import Logo from "./component/Logo"
+import NavMenu from "./component/NavMenu"
 
 export default Layout
+export {Breadcrumb, ContextMenu, Hamburger, HorizontalResizableMenu, Logo, NavMenu}
 
 export * from "./config"
 export * from "./store"
 
-export {refreshPage, closeCurrentPage}
+export {refreshPage, closeCurrentPage} from "./helper"
