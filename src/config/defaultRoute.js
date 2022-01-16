9fac84843d71e9510fa430800ed3bdd0b26f66fc
toesbieya <1647775459@qq.com>
2022-01-16 15:38:09 +0800

    Redirect组件不再单独列出

--- src/config/defaultRoute.js
+++ src/config/defaultRoute.js
@@ -1,5 +1,19 @@
 import Const from './const'
-import Redirect from '../component/Redirect'
+
+/**
+ * 用于刷新路由的工具组件
+ */
+const Redirect = {
+  mounted() {
+    const { fullPath } = this.$route
+    const to = fullPath.replace(new RegExp(Const.redirectPath, 'gm'), '')
+
+    // 如果路由没有component选项时（比如iframe路由），必须使用$nextTick模拟路由组件加载
+    this.$nextTick(() => this.$router.replace(to))
+  },
+
+  render: () => undefined
+}
 
 /**
  * 注入默认的路由
